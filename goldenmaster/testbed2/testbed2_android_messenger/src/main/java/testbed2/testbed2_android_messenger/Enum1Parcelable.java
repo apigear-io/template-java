71ca55587cffba2040864c91b9916ab3ed89c90e
Aleksander Wawrzyniak <aleksander.wawrzyniak@siili.com>
2026-01-28 14:09:20 +0100

    fix(parcel): handle null enums
    We want to prevent null execptions when writing to parcel.
    
    If the enum is null, calling enum.getValue() will cause a crash. Prevent
    it by parceling it only when it isn't null. Always parcel a null flag.

--- goldenmaster/testbed2/testbed2_android_messenger/src/main/java/testbed2/testbed2_android_messenger/Enum1Parcelable.java
+++ goldenmaster/testbed2/testbed2_android_messenger/src/main/java/testbed2/testbed2_android_messenger/Enum1Parcelable.java
@@ -23,7 +23,8 @@ import java.util.Arrays;
     }
 
     protected Enum1Parcelable(Parcel in) {
-        this.data = Enum1.fromValue(in.readInt());
+        boolean dataIsValid = in.readBoolean();
+        this.data = dataIsValid ? Enum1.fromValue(in.readInt()) : null;
     }
 
     public static final Creator<Enum1Parcelable> CREATOR = new Creator<Enum1Parcelable>() {
@@ -41,7 +42,10 @@ import java.util.Arrays;
     @Override
     public void writeToParcel(Parcel dest, int flags) {
 
-        dest.writeInt(data.getValue());
+        dest.writeBoolean(data != null);
+        if (data != null) {
+            dest.writeInt(data.getValue());
+        }
     }
 
     public static Enum1Parcelable[] wrapArray(Enum1[] enums) {
